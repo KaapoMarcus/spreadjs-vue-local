046f2f81b3be4fc2a9b73d7a5a87b67cc26bfc77
maqiang@zccninfo.com <cnmq2020>
2020-11-02 17:36:36 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <gc-spread-sheets-designer :styleInfo='styleInfo' :config='config' @designerInitialized='designerInitialized'>
+    </gc-spread-sheets-designer>
+  </div>
+</template>
+
+<script>
+
+  import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css'
+  import '@grapecity/spread-sheets-designer/styles/gc.spread.sheets.designer.min.css'
+  import '@grapecity/spread-sheets-designer-resources-cn';
+  import { Designer } from '@grapecity/spread-sheets-designer-vue'
+
+  export default {
+    name: 'App',
+    data: function () {
+      return {
+        styleInfo: { height: '800px', width: '1200px', border: 'solid red 1px' },
+        config: null,
+        designer: null
+      };
+    },
+    methods: {
+      designerInitialized(value) {
+        this.designer = value;
+      }
+    }
+  }
+</script>
+<style>
+</style>
